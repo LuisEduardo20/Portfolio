ed53361762b89032930f35df6eace39f59f71226
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-14 23:12:42 -0300

    Import dos ícones svg's e descrição do usuário

--- src/components/WhoAmI/index.tsx
+++ src/components/WhoAmI/index.tsx
@@ -1,19 +1,53 @@
-import { Container, Description, Skills } from "./style";
+import {
+  Container,
+  UserSection,
+  Description,
+  Title,
+  Skills,
+  Picture,
+  IconsArea,
+} from "./style";
+
+import css from "../../assets/css.svg";
+import figma from "../../assets/figma.svg";
+import git from "../../assets/git.svg";
+import github from "../../assets/github.svg";
+import heroku from "../../assets/html.svg";
+import html from "../../assets/html.svg";
+import js from "../../assets/js.svg";
+import linux from "../../assets/linux.svg";
+import mysql from "../../assets/mysql.svg";
+import node from "../../assets/node.svg";
+import postgres from "../../assets/postgres.svg";
+import react from "../../assets/react.svg";
+import ts from "../../assets/ts.svg";
+import vscode from "../../assets/vscode.svg";
+import wordpress from "../../assets/wordpress.svg";
 
 const WhoAmI = () => {
   return (
     <Container>
-      <Description>
-        <h4>Quem sou eu?</h4>
+      <UserSection>
+        <Title>Quem sou eu?</Title>
 
-        <img src='' alt='' />
+        <Picture
+          src='https://cdn.discordapp.com/attachments/914144800050143242/942961177837858886/foto_perfil.jpeg'
+          alt='Foto de perfil'
+        />
 
-        <div>
-          <p>Minha descrição</p>
-        </div>
-      </Description>
+        <Description>
+          <p>
+            Olá, me chamo Eduardo, sou desenvolvedor Full Stack React e Node,
+            estudante de Sistemas de Informação e apaixonado por aprender!
+          </p>
+        </Description>
+      </UserSection>
 
-      <Skills></Skills>
+      <Skills>
+        <Title>Habilidades</Title>
+
+        <IconsArea></IconsArea>
+      </Skills>
     </Container>
   );
 };
--- src/components/WhoAmI/style.ts
+++ src/components/WhoAmI/style.ts
@@ -8,12 +8,67 @@ export const Container = styled.div`
   background-color: var(--white);
 
   display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const UserSection = styled.div`
+  width: 50%;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  padding: 20px 0 20px 0;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 export const Description = styled.div`
-  width: 50%;
+  width: 80%;
+
+  padding: 20px;
+
+  border-radius: 10px;
+
+  background-color: var(--black);
+  color: var(--white);
 `;
 
 export const Skills = styled.div`
   width: 50%;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const IconsArea = styled.div``;
+
+export const Icon = styled.img``;
+
+export const Title = styled.h4`
+  color: var(--black);
+  font-weight: 600;
+  font-size: 1.5rem;
+  text-decoration: underline;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.25rem;
+  }
+`;
+
+export const Picture = styled.img`
+  max-width: 50%;
+  border-radius: 50%;
 `;
