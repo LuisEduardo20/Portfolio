f7a0733327f48574fe6e353a5b00b4d98f011edf
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-15 22:59:08 -0300

    adição dos paddings verticais

--- src/components/Main/style.ts
+++ src/components/Main/style.ts
@@ -5,5 +5,11 @@ export const Container = styled.main`
   flex-direction: column;
   align-items: center;
 
+  padding: 40px 0 40px 0;
+
   background-color: var(--black);
+
+  @media screen and (max-width: 768px) {
+    padding: 20px 0 20px 0;
+  }
 `;
