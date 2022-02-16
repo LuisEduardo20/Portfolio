2b0fce351a6be55722c595c89e5427f9d714b35b
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-16 16:21:25 -0300

    Corpo do formulário de contato

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,37 @@
+import { Container, Title, Form, Input, Text, Submit } from "./style";
+
+import gmailIcon from "../../assets/gmail.svg";
+import linkedinIcon from "../../assets/linkedin.svg";
+
 const Contact = () => {
-  return <div></div>;
+  return (
+    <Container id='contato'>
+      <Title>Contato</Title>
+
+      <Form>
+        <Input type='text' placeholder='Nome' />
+
+        <Input type='text' placeholder='Email' />
+
+        <Input type='text' placeholder='Assunto' />
+
+        <Text rows={7} placeholder='Texto'></Text>
+
+        <Submit>
+          Enviar
+          <img src={gmailIcon} alt='Ícone do Gmail' />
+        </Submit>
+
+        <a
+          href='https://www.linkedin.com/in/luis-eduardo-santos-costa-5369841b3/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img src={linkedinIcon} alt='Ícone do Linkedin' />
+        </a>
+      </Form>
+    </Container>
+  );
 };
 
 export default Contact;
---
+++ src/components/Contact/style.ts
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 35%;
+  height: 850px;
+  margin: 40px 0 40px 0;
+
+  background-color: var(--white);
+  border-radius: 10px;
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 10% 1fr;
+
+  @media screen and (max-width: 768px) {
+    width: 90%;
+
+    margin: 20px 0 20px 0;
+    padding: 20px;
+
+    grid-template-rows: 8% 1fr;
+  }
+`;
+
+export const Title = styled.h2`
+  margin-top: 20px;
+
+  color: var(--black);
+  font-weight: 600;
+  font-size: 2rem;
+  text-decoration: underline;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 0px;
+  }
+`;
+
+export const Form = styled.form`
+  width: 100%;
+  /* background-color: red; */
+
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  width: 80%;
+  height: 60px;
+
+  font-size: 1.45rem;
+  text-indent: 20px;
+
+  color: var(--black);
+  background-color: var(--input-background);
+
+  border: none;
+  border-radius: 10px;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: var(--input-placeholder);
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Text = styled.textarea`
+  width: 80%;
+  font-size: 1.45rem;
+  padding: 20px;
+
+  color: var(--black);
+  background-color: var(--input-background);
+
+  border: none;
+  border-radius: 10px;
+  box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.25);
+
+  &:focus {
+    outline: none;
+  }
+
+  &::placeholder {
+    color: var(--input-placeholder);
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+export const Submit = styled.button`
+  width: 80%;
+  height: 60px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-size: 1.5rem;
+
+  color: var(--white);
+  background-color: var(--button-background);
+  border: 0;
+  border-radius: 10px;
+
+  cursor: pointer;
+
+  transition: 0.2s;
+
+  &:hover {
+    background-color: var(--black);
+  }
+
+  & > img {
+    margin-left: 20px;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+
+    & > img {
+      margin-left: 10px;
+    }
+  }
+`;
