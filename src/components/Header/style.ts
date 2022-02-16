cc55af5aaefd2e98691ef65d0ce4ca23dd5f07d8
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-16 08:28:45 -0300

    Adição do position sitcky

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: -webkit-sticky;
+  position: sticky;
+  top: 0;
+
   display: flex;
   justify-content: center;
 
