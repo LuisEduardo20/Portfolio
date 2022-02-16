cb34a21d8437e6361416c1cc8389980ad88d8268
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-16 08:34:26 -0300

    Hide 'X' scroll-bar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,8 +13,12 @@ export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    
     box-sizing: border-box;
+    
     font-family: 'Poppins', sans-serif;
+
+    overflow-x: hidden;
   }
 
   body {
