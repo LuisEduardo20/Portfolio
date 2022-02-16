dfc322142a8755cd52690fe6473fd5b2bc1c0ab4
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-16 12:33:45 -0300

    Adição das responsividades de font no HTML

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -21,6 +21,20 @@ export const GlobalStyle = createGlobalStyle`
     overflow-x: hidden;
   }
 
+  html {
+    @media (max-width: 1080px) {
+      //? 93.75% de 16px igual 15px;
+      //? ideal para acessibilidade em dispositivos que estão com a fonte maior ou menor
+      font-size: 93.75%;
+    }
+    
+    @media (max-width: 720px) {
+      //? 87.5% de 16px igual 14px;
+      //? ideal para acessibilidade em dispositivos que estão com a fonte maior ou menor
+      font-size: 87.5%;
+    }
+  }
+
   body {
     width: 100vw;
     height: 100vh;
