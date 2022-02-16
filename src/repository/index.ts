b8a33fce5e10e971876c6a2ed4ce7a407a223433
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-16 12:33:23 -0300

    Repositório de projetos

---
+++ src/repository/index.ts
@@ -0,0 +1,32 @@
+export const repositories = [
+  {
+    name: "NUTES_Coronavirus",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943504746017329232/unknown.png",
+    description:
+      "Portal para informar a população sobre a COVID-19 e redirecionar para tele-atendimentos gratuitos",
+    applicationLink: "https://nutes.ufpe.br/coronavirus/",
+    repositoryLink: "",
+  },
+
+  {
+    name: "GitHub Searcher",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943504434451849308/unknown.png",
+    description:
+      "Um pesquisador de usuários do GitHub onde é possível ver informações como repositórios públicos, quantidade de seguidores e etc...",
+    applicationLink: "https://github-searcher-luiseduardo20.vercel.app/",
+    repositoryLink:
+      "https://github.com/LuisEduardo20/desafio_estagio-ClickSoft/tree/main/front_web",
+  },
+
+  {
+    name: "SUPER 2021",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943504930784829450/unknown.png",
+    description:
+      "SITE de apresentação da SUPER que é um evento online instituido pela UPE Caruaru para promover a iniciação científica e impulsionar a cultura local",
+    applicationLink: "https://caruaru.upe.br/super/",
+    repositoryLink: "",
+  },
+];
