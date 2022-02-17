14aed43384658534460e7500a01c31bf87dae827
LuisEduardo20 <luiseduardo20.dev@gmail.com>
2022-02-17 17:36:55 -0300

    +Projetos API GOT, Cronômetro e portifólio

--- src/repository/index.ts
+++ src/repository/index.ts
@@ -1,11 +1,30 @@
 export const repositories = [
   {
+    //!Template
+    // {
+    //   name: "",
+    //   imgUrl: "",
+    //   description: "",
+    //   applicationLink: "",
+    //   repositoryLink: "",
+    // },
+
     name: "NUTES_Coronavirus",
     imgUrl:
       "https://cdn.discordapp.com/attachments/914144800050143242/943504746017329232/unknown.png",
     description:
-      "Portal para informar a população sobre a COVID-19 e redirecionar para tele-atendimentos gratuitos",
+      "Portal construído pelo NUTES(Núcleo de telessaúde do Hospital das Clínicas) para informar a população sobre a COVID-19 e redirecionar para tele-atendimentos gratuitos.",
     applicationLink: "https://nutes.ufpe.br/coronavirus/",
+    repositoryLink: "",
+  },
+
+  {
+    name: "SUPER 2021",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943504930784829450/unknown.png",
+    description:
+      "SITE de apresentação da SUPER que é um evento online instituido pela UPE Caruaru para promover a iniciação científica e impulsionar a cultura local",
+    applicationLink: "https://caruaru.upe.br/super/",
     repositoryLink: "",
   },
 
@@ -21,12 +40,32 @@ export const repositories = [
   },
 
   {
-    name: "SUPER 2021",
+    name: "Game of Thrones API",
     imgUrl:
-      "https://cdn.discordapp.com/attachments/914144800050143242/943504930784829450/unknown.png",
+      "https://cdn.pixabay.com/photo/2021/02/09/23/31/stark-6000364_960_720.jpg",
     description:
-      "SITE de apresentação da SUPER que é um evento online instituido pela UPE Caruaru para promover a iniciação científica e impulsionar a cultura local",
-    applicationLink: "https://caruaru.upe.br/super/",
-    repositoryLink: "",
+      "Uma API feita em Typescript com Express que armazena dados dos principais personagens da série de TV como: nome, livros em que participa, casas a que pertence etc...",
+    applicationLink: "https://desafio-mobix-backend.herokuapp.com/characters",
+    repositoryLink: "https://github.com/LuisEduardo20/desafio_mobix-backend",
+  },
+
+  {
+    name: "Cronômetro Web",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943962548154302565/unknown.png",
+    description:
+      "Um cronômetro feito com HTML, CSS e JavaScript como forma de estudo e práticas de novos conhecimentos.",
+    applicationLink: "https://web-chronometer-six.vercel.app/",
+    repositoryLink: "https://github.com/LuisEduardo20/Web-chronometer",
+  },
+
+  {
+    name: "Meu Portifólio",
+    imgUrl:
+      "https://cdn.discordapp.com/attachments/914144800050143242/943966624933900358/unknown.png",
+    description:
+      "Meu portifólio construído com ReactJs, TypeScript e protótipo no Figma.",
+    applicationLink: "https://portifolio-luiseduardo20.vercel.app/",
+    repositoryLink: "https://github.com/LuisEduardo20/Portifolio",
   },
 ];
